fc13cd4252e26e6cf6a88d0ea55ce3ff231b931a
vinay <58105215+vinay-ardhani01010@users.noreply.github.com>
2020-10-03 20:34:52 +0530

    Update messages_routes.js

--- backend/routes/messages_routes.js
+++ backend/routes/messages_routes.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const MessageSchema = require("../models/messages_models");
 
 const router = express.Router();
 const messagesControllers=require('../controllers/messages_controllers');
 
 
 //gets all messages
-router.get('/',messagesControllers.getMessages);
+router.post("/sendmessage",(req,res)=>{
+  const status = req.body.status;
+  const message = req.body.message;
+ const  newMessage = new MessageSchema({
+     status,
+     message
+ });
+  newMessage.save()
+        .then(() => console.log("added"))
+        .catch(err => res.status(400).json('Error: '+ err));
+})
+router.get('/showmessages',messagesControllers.getMessages);
 module.exports = router;
