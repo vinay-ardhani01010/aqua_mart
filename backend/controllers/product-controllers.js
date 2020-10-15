707a5503a736c3ce8b09d77e1fa82caee44f4875
rohan2734 <jagandevaki1@gmail.com>
2020-10-15 16:46:04 +0530

    removed errors

--- backend/controllers/product-controllers.js
+++ backend/controllers/product-controllers.js
@@ -4,7 +4,7 @@ const Product = require('../models/product.model');
 //get products from vendor for admin
 //route- getProductsFromVendorsForAdmin
 
-const getProductsFromVendorsForAdmin = (req,res,next) => {
+const getProductsFromVendorForAdmin = (req,res,next) => {
   const vendorId = req.params.vendorId;
   Product.find({vendorId})
   .then(products => {
@@ -13,4 +13,4 @@ const getProductsFromVendorsForAdmin = (req,res,next) => {
 
 }
 
-exports.getProductsFromVendorsForAdmin = getProductsFromVendorsForAdmin;
+exports.getProductsFromVendorForAdmin = getProductsFromVendorForAdmin;
